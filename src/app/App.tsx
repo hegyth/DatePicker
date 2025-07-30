2cfb6c021100a3bb1d16e300cf197e7b304386eb
Смирнов Артём Сергеевич <ASmirnov266@alfabank.ru>
2025-07-31 01:17:29 +0300

    init

---
+++ src/app/App.tsx
@@ -0,0 +1,17 @@
+import "../styles/App.scss";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <button>Click me</button>
+      <div className="box">
+        <h2>Hello World</h2>
+        <p>This is a paragraph</p>
+        <button>Click me</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
